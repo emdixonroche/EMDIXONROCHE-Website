41c89ab614677b975f101c6e0d4e00dff3a6f9a3
Eleonore Dixon-Roche <emdixonroche@gmail.com>
2019-07-31 11:10:46 -0400

    Added pages to final personal website

--- send_mail.php
+++ send_mail.php
@@ -5,11 +5,17 @@ You will need to change this value to a valid email address that you can access.
 */
 $webmaster_email = "info@emdixonroche.com";
 
+
 /*
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
 $hireme_page = "hireme.html";
+
+// TODO: update these
+$feedback_page = "hireme.html";
+$error_page = "hireme.html";
+$thankyou_page = "hireme.html";
 
 
 /*
@@ -22,7 +28,9 @@ $yourEmail = $_REQUEST['yourEmail'] ;
 $industry = $_REQUEST['industry'] ;
 $duration = $_REQUEST['duration'] ;
 $price = $_REQUEST['price'] ;
-$typeOfWork = $_REQUEST['typeOfWork'] ;
+$illustration = $_REQUEST['Illustration'];
+$ui = $_REQUEST['UI'];
+$webtemplate = $_REQUEST['WebTemplate'];
 $comments = $_REQUEST['comments'] ;
 
 
@@ -33,48 +41,55 @@ $msg =
 "Industry: " . $industry . "\r\n" .
 "Project Duration: " . $duration . "\r\n" .
 "Budget: " . $price . "\r\n" .
-"Type of Work: " . $typeOfWork . "\r\n" .
+"Type of work" . "\r\n" .
+"Illustration: " . $illustration . "\r\n" .
+"UI: " . $ui . "\r\n" .
+"WebTemplate: " . $webtemplate . "\r\n" .
 "Comments: " . $comments ;
 
-?>
 
 
 
 
 
 
-// /*
-// The following function checks for email injection.
-// Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
-// */
-// function isInjected($str) {
-// 	$injections = array('(\n+)',
-// 	'(\r+)',
-// 	'(\t+)',
-// 	'(%0A+)',
-// 	'(%0D+)',
-// 	'(%08+)',
-// 	'(%09+)'
-// 	);
-// 	$inject = join('|', $injections);
-// 	$inject = "/$inject/i";
-// 	if(preg_match($inject,$str)) {
-// 		return true;
-// 	}
-// 	else {
-// 		return false;
-// 	}
-// }
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 // // If the user tries to access this script directly, redirect them to the feedback form,
 // if (!isset($_REQUEST['email_address'])) {
 // header( "Location: $feedback_page" );
 // }
 
+// echo $_REQUEST['email_address']
+// echo !isset($_REQUEST['email_address'])
+
 // // If the form fields are empty, redirect to the error page.
 // elseif (empty($first_name) || empty($email_address)) {
 // header( "Location: $error_page" );
 // }
+
 
 // /* 
 // If email injection is detected, redirect to the error page.
@@ -87,7 +102,10 @@ $msg =
 // // If we passed all previous tests, send the email then redirect to the thank you page.
 // else {
 
-// 	mail( "$webmaster_email", "Feedback Form Results", $msg );
 
-// 	header( "Location: $thankyou_page" );
+	mail( "$webmaster_email", "Feedback Form Results", $msg );
+
+	header( "Location: $thankyou_page" );
 // }
+
+?>
